bc5f36a4b7eb967739937c26ddb4c338a7b416ed
Ahmed Magdy <ahmedmagdi@outlook.com>
2019-09-29 21:40:54 +0200

    add spotify response model

---
+++ models/spotifty-response.go
@@ -0,0 +1,30 @@
+package models
+
+// Track  object response for track
+type Track struct {
+	ID     string `json:"id"`
+	URL    string `json:"href"`
+	Images []struct {
+		URL string `json:"url"`
+	} `json:"images"`
+	Name    string   `json:"name"`
+	Artists []Artist `json:"artists"`
+}
+
+type Artist struct {
+	Name string `json:"name"`
+}
+
+// MeTrackResponse  /me/track response
+type MeTrackResponse struct {
+	Href string `json:"href"`
+
+	Items []struct {
+		Track Track `json:"track"`
+	} `json:"items"`
+
+	Previous string `json:"previous"`
+	Next     string `json:"next"`
+	Offset   string `json:"offset"`
+	Total    string `json:"total"`
+}
